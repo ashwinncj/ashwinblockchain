d42973593b0f34b91b676a5f63af94f918cfc9e4
Ashwin Kumar C <ashwinncj@gmail.com>
2019-09-05 23:11:10 +0530

    05/09/2019	23:11:10

--- v0.1/startBlockchain.sh
+++ v0.1/startBlockchain.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cd src/ && npm install
+echo "Starting AshwinBlockchain..."
+echo "Checking node packages and updating modules"
+cd src/ && npm install > /dev/null 2>&1
+echo "Attempting to bring up AshwinBlockchain servers"
 docker-compose up
