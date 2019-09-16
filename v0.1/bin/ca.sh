f76a4253a6c43fe4d987efa1d77cd6892af3f681
Ashwin Kumar C <ashwinncj@gmail.com>
2019-09-16 18:06:16 +0530

    16/09/2019	18:06:16

--- v0.1/bin/ca.sh
+++ v0.1/bin/ca.sh
@@ -25,6 +25,63 @@ caSetup(){
     openssl req -x509 -key $ABCCONFIG/ca/$CANAME/$CANAME.pem -new -out $ABCCONFIG/ca/$CANAME/$CANAME.crt -days 365
 }
 
+#Function for requesting certificate from the CA
+caRequest(){
+    MEMBER=$1
+    if [ -z ${MEMBER+x} ]; then #Checking if the variable MEMBER is unset
+    echo "Error: Please provide a name for the requesting member."
+    exit 1
+    fi
+
+    #Check if member already exists
+     if [ -d $ABCCONFIG/member/$MEMBER ]; then 
+    echo "Error: Network member \"$MEMBER\" already exists"
+    exit 1
+    fi
+
+    #Create a directory in the name of the member
+    echo "Setting up network member \"$MEMBER\""
+    {
+        mkdir -p $ABCCONFIG/member/$MEMBER
+        openssl genrsa -out $ABCCONFIG/member/$MEMBER/$MEMBER.pem
+    }&> /dev/null
+
+    openssl req -key $ABCCONFIG/member/$MEMBER/$MEMBER.pem -new -out $MEMBER.csr
+    echo ""
+    echo "Certificate Signing Request (CSR) for \"$MEMBER\" has been output in the present directory as \"$MEMBER.csr\" . Please send the file to CA to sign and obtain the certificate to join Ashwin Blockchain Network."
+}
+
+#Function to sign the CSR from using the CA keys
+caSign(){
+CANAME=$1
+    if [ -z ${CANAME+x} ]; then #Checking if the variable CNAME is unset
+    echo "Error: Please provide a name for the CA."
+    exit 1
+    fi
+
+    #Check if CA exists
+     if [ ! -d $ABCCONFIG/ca/$CANAME ]; then 
+    echo "Error: CA \"$CANAME\" does not exist. Please check the CA name"
+    exit 1
+    fi
+
+    MEMBER=$2
+    if [ -z ${MEMBER+x} ]; then #Checking if the variable MEMBER is unset
+    echo "Error: Please provide a valid CSR file to obtain the certificate."
+    exit 1
+    fi
+
+    openssl x509 -in $MEMBER -req -CA $ABCCONFIG/ca/$CANAME/$CANAME.crt -CAkey $ABCCONFIG/ca/$CANAME/$CANAME.pem -CAcreateserial -out $MEMBER.crt -days 365
+
+    if [ -f ./$MEMBER.crt ]; then
+        echo "Certificate for \"$MEMBER\" is output in the present directory as \"$MEMBER.crt\" . Send the certificate file to the member to enable them to join the Ashwin Blockchain network."        
+    else
+        echo "There seems to be an error. Please check with the manual for the usage."
+    fi
+}
+
+#User input recorded from the terminal to start the CA module.
+
 if [ -z ${1+x} ]; then # Check if the operation mode is set
     echo "Error: Please provide the operation mode for the CA"
     exit 1
@@ -36,9 +93,13 @@ case "$mode" in
     setup)
         caSetup $2
     ;;
-    2|3)
-        echo "case 2 or 3"
+    request)
+        caRequest $2
     ;;
+    sign)
+        caSign $2 $3
+    ;;
+    
     *)
         echo "default"
     ;;
